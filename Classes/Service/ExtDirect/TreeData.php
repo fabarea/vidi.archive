591538e1735d45779a1db02041eab29056f00efd
Steffen Ritter <info@rs-websystems.de>
2011-09-30 11:46:37 +0200

    [TASK] adapted TreeData Provider according to the Core Tree API changes

--- Classes/Service/ExtDirect/TreeData.php
+++ Classes/Service/ExtDirect/TreeData.php
@@ -7,25 +7,28 @@ class Tx_Vidi_Service_ExtDirect_TreeData extends Tx_Vidi_Service_ExtDirect_Abstr
 		$this->loadConfiguration($parameters->moduleCode);
 		$treeId = $parameters->tree;
 
-		/** @var t3lib_tree_Tca_ExtJsArrayRenderer $renderer s*/
-		$renderer = t3lib_div::makeInstance('t3lib_tree_Tca_ExtJsArrayRenderer');
+		/** @var t3lib_tree_Renderer_ExtJsJson $renderer */
+		$renderer = t3lib_div::makeInstance('t3lib_tree_Renderer_ExtJsJson');
 
 		/** @var t3lib_tree_Tca_DatabaseTreeDataProvider $dataProvider */
 		$dataProvider = t3lib_div::makeInstance('t3lib_tree_Tca_DatabaseTreeDataProvider');
 
 		$dataProvider->setLookupField('pid');
-		$dataProvider->setRootUid($parameters->node);
-		$dataProvider->setNonSelectableLevelList('0,1');
+		$dataProvider->setRootUid(0);
+		$dataProvider->setNonSelectableLevelList('*');
 		$dataProvider->setLookupMode(t3lib_tree_Tca_DatabaseTreeDataProvider::MODE_PARENT);
 
 		$dataProvider->setTableName($this->moduleConfiguration['trees'][$treeId]['table']);
 		$dataProvider->setLabelField('title');
 		$dataProvider->setExpandAll(false);
 		$dataProvider->initializeTreeData();
-		return $renderer->renderNodeCollection(
-			$dataProvider->getRoot()->getChildNodes(),
-			false
-		);
+
+
+		$requestedNode = $dataProvider->renderNode($parameters->node, 1);
+		if ($requestedNode != null && ($nodeData = $requestedNode->getChildNodes()) != null) {
+			return $renderer->renderNodeCollection($nodeData);
+		}
+		return array();
 	}
 
 
