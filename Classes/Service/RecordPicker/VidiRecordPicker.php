7c3d2370a26d9c01384dd1c8cb534a666ed5ec5b
Steffen Ritter <info@rs-websystems.de>
2012-01-09 08:07:08 +0100

    [TASK] remove Vidi Picker for COllections

--- Classes/Service/RecordPicker/VidiRecordPicker.php
+++ Classes/Service/RecordPicker/VidiRecordPicker.php
@@ -8,7 +8,7 @@ class Tx_Vidi_Service_RecordPicker_VidiRecordPicker {
 	 * @return bool
 	 */
 	public function canHandle($mode, $allowed) {
-		return $mode === 'db' && (strpos($allowed,'sys_file') !== FALSE || strpos($allowed, 'sys_file_collection') !== FALSE);
+		return $mode === 'db' && (strpos($allowed,'sys_file') !== FALSE);
 	}
 
 	// TODO register media and file somehow
